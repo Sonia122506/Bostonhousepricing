1c24862a5f4b66d37fe6dc94ade394f048cfaaa9
Sonia122506 <soniay0510@gmail.com>
2023-11-14 17:50:30 +0530

    Adding Prockfile

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ model=pickle.load(open("regmodel.pkl","rb"))
 def home():
     return render_template("home.html")
 
-@app.route("/predict_api",method=["POST"])
+@app.route("/predict_api",methods=["POST"])
 def predict_api():
     data=request.json["data"]
     print(data)
@@ -19,5 +19,15 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+@app.route("/predict",methods=["POST"])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input[0])
+    render_template("home.html",prediction_text="The House Price predicted is {}".format(output))
+
 if __name__=="__main__":
     app.run(debug=True)
+
+
